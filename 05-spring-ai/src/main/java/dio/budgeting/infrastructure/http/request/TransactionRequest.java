1eb304b81520ab798e008d3d290cdefe0c7f4c0e
lucasmbc <lucasmbc@hotmail.com>
2026-09-05 16:22:37 -0300

    feat: add validation annotations to TransactionRequest fields

--- 05-spring-ai/src/main/java/dio/budgeting/infrastructure/http/request/TransactionRequest.java
+++ 05-spring-ai/src/main/java/dio/budgeting/infrastructure/http/request/TransactionRequest.java
@@ -2,8 +2,20 @@ package dio.budgeting.infrastructure.http.request;
 
 import dio.budgeting.application.input.PersistTransactionInput;
 import dio.budgeting.domain.Category;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-public record TransactionRequest(String description, Category category, long amount) {
+public record TransactionRequest(
+        @NotBlank(message = "Descrição inválida.")
+        String description,
+
+        @NotNull(message = "Categoria inválida.")
+        Category category,
+
+        @Positive(message = "Valor inválido! Verifique se o valor é positivo e maior que zero.")
+        Long amount
+) {
     public PersistTransactionInput toInput() {
         return new PersistTransactionInput(description, amount, category);
     }
